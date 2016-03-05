a02c717d298de1e5a91d80b30889dff7b89a661b
Aster Atwood <strcatyou@gmail.com>
2016-03-05 19:28:44 +0900

    defined Graphics

--- rpg_core.d.ts
+++ rpg_core.d.ts
@@ -3,11 +3,625 @@
 // Definitions by: aaa<https://>, bbb<https://>
 // Definitions: https://
 
+// You must be Uncomment if not reference pixi.d.ts.
+// declare namespace PIXI {
+//     // aaa
+// }
 
+/**
+ * The static class that carries out graphics processing.
+ *
+ * @class Graphics
+ */
 interface GraphicsStatic {
-    render(stage: Stage);
+    /**
+     * The total frame count of the game screen.
+     *
+     * @static
+     * @property frameCount
+     * @type Number
+     */
+    frameCount: number;
+
+    /**
+     * The alias of PIXI.blendModes.NORMAL.
+     *
+     * @static
+     * @property BLEND_NORMAL
+     * @type Number
+     * @final
+     */
+    BLEND_NORMAL: PIXI.blendModes;
+
+    /**
+     * The alias of PIXI.blendModes.ADD.
+     *
+     * @static
+     * @property BLEND_ADD
+     * @type Number
+     * @final
+     */
+    BLEND_ADD: PIXI.blendModes;
+
+    /**
+     * The alias of PIXI.blendModes.MULTIPLY.
+     *
+     * @static
+     * @property BLEND_MULTIPLY
+     * @type Number
+     * @final
+     */
+    BLEND_MULTIPLY: PIXI.blendModes;
+
+    /**
+     * The alias of PIXI.blendModes.SCREEN.
+     *
+     * @static
+     * @property BLEND_SCREEN
+     * @type Number
+     * @final
+     */
+    BLEND_SCREEN: PIXI.blendModes;
+
+    /**
+     * The width of the game screen.
+     *
+     * @static
+     * @property width
+     * @type Number
+     */
+    width: number;
+
+    /**
+     * The height of the game screen.
+     *
+     * @static
+     * @property height
+     * @type Number
+     */
+    height: number;
+
+    /**
+     * The width of the window display area.
+     *
+     * @static
+     * @property boxWidth
+     * @type Number
+     */
+    boxWidth: number;
+
+    /**
+     * The height of the window display area.
+     *
+     * @static
+     * @property boxHeight
+     * @type Number
+     */
+    boxHeight: number;
+
+    /**
+     * The zoom scale of the game screen.
+     *
+     * @static
+     * @property scale
+     * @type Number
+     */
+    scale: number;
+
+    /**
+     * Initializes the graphics system.
+     *
+     * @static
+     * @method initialize
+     * @param {Number} width The width of the game screen
+     * @param {Number} height The height of the game screen
+     * @param {String} type The type of the renderer.
+     *                 'canvas', 'webgl', or 'auto'.
+     */
+    initialize(): void;
+    initialize(width: number, height: number, type: string): void;
+
+    /**
+     * Marks the beginning of each frame for FPSMeter.
+     *
+     * @static
+     * @method tickStart
+     */
+    tickStart(): void;
+
+    /**
+     * Marks the end of each frame for FPSMeter.
+     *
+     * @static
+     * @method tickEnd
+     */
+    tickEnd(): void;
+
+    /**
+     * Renders the stage to the game screen.
+     *
+     * @static
+     * @method render
+     * @param {Stage} stage The stage object to be rendered
+     */
+    render(stage?: Stage): void;
+
+    /**
+     * Checks whether the renderer type is WebGL.
+     *
+     * @static
+     * @method isWebGL
+     * @return {Boolean} True if the renderer type is WebGL
+     */
+    isWebGL(): boolean;
+
+    /**
+     * Checks whether the current browser supports WebGL.
+     *
+     * @static
+     * @method hasWebGL
+     * @return {Boolean} True if the current browser supports WebGL.
+     */
+    hasWebGL(): boolean;
+
+    /**
+     * Checks whether the canvas blend mode 'difference' is supported.
+     *
+     * @static
+     * @method canUseDifferenceBlend
+     * @return {Boolean} True if the canvas blend mode 'difference' is supported
+     */
+    canUseDifferenceBlend(): boolean;
+
+    /**
+     * Checks whether the canvas blend mode 'saturation' is supported.
+     *
+     * @static
+     * @method canUseSaturationBlend
+     * @return {Boolean} True if the canvas blend mode 'saturation' is supported
+     */
+    canUseSaturationBlend(): boolean;
+
+    /**
+     * Sets the source of the "Now Loading" image.
+     *
+     * @static
+     * @method setLoadingImage
+     * @param {String} Path of image
+     */
+    setLoadingImage(src: string);
+
+    /**
+     * Initializes the counter for displaying the "Now Loading" image.
+     *
+     * @static
+     * @method startLoading
+     */
+    startLoading(): void;
+
+    /**
+     * Increments the loading counter and displays the "Now Loading" image if necessary.
+     *
+     * @static
+     * @method updateLoading
+     */
+    updateLoading(): void;
+
+    /**
+     * Erases the "Now Loading" image.
+     *
+     * @static
+     * @method endLoading
+     */
+    endLoading(): void;
+
+    /**
+     * Displays the error text to the screen.
+     *
+     * @static
+     * @method printError
+     * @param {String} name The name of the error
+     * @param {String} message The message of the error
+     */
+    printError(name: string, message: string): void;
+
+    /**
+     * Shows the FPSMeter element.
+     *
+     * @static
+     * @method showFps
+     */
+    showFps(): void;
+
+    /**
+     * Hides the FPSMeter element.
+     *
+     * @static
+     * @method hideFps
+     */
+    hideFps(): void;
+
+    /**
+     * Loads a font file.
+     *
+     * @static
+     * @method loadFont
+     * @param {String} name The face name of the font
+     * @param {String} url The url of the font file
+     */
+    loadFont(name: string, url: string): void;
+
+    /**
+     * Checks whether the font file is loaded.
+     *
+     * @static
+     * @method isFontLoaded
+     * @param {String} name The face name of the font
+     * @return {Boolean} True if the font file is loaded
+     */
+    isFontLoaded(name: string): boolean;
+
+    /**
+     * Starts playback of a video.
+     *
+     * @static
+     * @method playVideo
+     * @param {String} src
+     */
+    playVideo(src: string): void;
+
+    /**
+     * Checks whether the video is playing.
+     *
+     * @static
+     * @method isVideoPlaying
+     * @return {Boolean} True if the video is playing
+     */
+    isVideoPlaying(): boolean;
+
+    /**
+     * Checks whether the browser can play the specified video type.
+     *
+     * @static
+     * @method canPlayVideoType
+     * @param {String} type The video type to test support for
+     * @return {Boolean} True if the browser can play the specified video type
+     */
+    canPlayVideoType(type: string): boolean;
+
+    /**
+     * Converts an x coordinate on the page to the corresponding
+     * x coordinate on the canvas area.
+     *
+     * @static
+     * @method pageToCanvasX
+     * @param {Number} x The x coordinate on the page to be converted
+     * @return {Number} The x coordinate on the canvas area
+     */
+    pageToCanvasX(x: number): number;
+
+    /**
+     * Converts a y coordinate on the page to the corresponding
+     * y coordinate on the canvas area.
+     *
+     * @static
+     * @method pageToCanvasY
+     * @param {Number} y The y coordinate on the page to be converted
+     * @return {Number} The y coordinate on the canvas area
+     */
+    pageToCanvasY(y: number): number;
+
+    /**
+     * Checks whether the specified point is inside the game canvas area.
+     *
+     * @static
+     * @method isInsideCanvas
+     * @param {Number} x The x coordinate on the canvas area
+     * @param {Number} y The y coordinate on the canvas area
+     * @return {Boolean} True if the specified point is inside the game canvas area
+     */
+    isInsideCanvas(x: number, y: number): boolean;
+
+    /**
+     * @static
+     * @method _createAllElements
+     * @private
+     */
+    _createAllElements(): void;
+
+    /**
+     * @static
+     * @method _updateAllElements
+     * @private
+     */
+    _updateAllElements(): void;
+
+    /**
+     * @static
+     * @method _updateRealScale
+     * @private
+     */
+    _updateRealScale(): void;
+
+    /**
+     * @static
+     * @method _makeErrorHtml
+     * @param {String} name
+     * @param {String} message
+     * @return {String}
+     * @private
+     */
+    _makeErrorHtml(name: string, message: string): void;
+
+    /**
+     * @static
+     * @method _defaultStretchMode
+     * @private
+     */
+    _defaultStretchMode(): void;
+
+    /**
+     * @static
+     * @method _testCanvasBlendModes
+     * @private
+     */
+    _testCanvasBlendModes(): void;
+
+    /**
+     * @static
+     * @method _modifyExistingElements
+     * @private
+     */
+    _modifyExistingElements(): void;
+
+    /**
+     * @static
+     * @method _createErrorPrinter
+     * @private
+     */
+    _createErrorPrinter(): void;
+
+    /**
+     * @static
+     * @method _updateErrorPrinter
+     * @private
+     */
+    _updateErrorPrinter(): void;
+
+    /**
+     * @static
+     * @method _createCanvas
+     * @private
+     */
+    _createCanvas(): void;
+
+    /**
+     * @static
+     * @method _updateCanvas
+     * @private
+     */
+    _updateCanvas(): void;
+
+    /**
+     * @static
+     * @method _createVideo
+     * @private
+     */
+    _createVideo(): void;
+
+    /**
+     * @static
+     * @method _updateVideo
+     * @private
+     */
+    _updateVideo(): void;
+
+    /**
+     * @static
+     * @method _createUpperCanvas
+     * @private
+     */
+    _createUpperCanvas(): void;
+
+    /**
+     * @static
+     * @method _updateUpperCanvas
+     * @private
+     */
+    _updateUpperCanvas(): void;
+
+    /**
+     * @static
+     * @method _clearUpperCanvas
+     * @private
+     */
+    _clearUpperCanvas(): void;
+
+    /**
+     * @static
+     * @method _paintUpperCanvas
+     * @private
+     */
+    _paintUpperCanvas(): void;
+
+    /**
+     * @static
+     * @method _createRenderer
+     * @private
+     */
+    _createRenderer(): void;
+
+    /**
+     * @static
+     * @method _updateRenderer
+     * @private
+     */
+    _updateRenderer(): void;
+
+    /**
+     * @static
+     * @method _createFPSMeter
+     * @private
+     */
+    _createFPSMeter(): void;
+
+    /**
+     * @static
+     * @method _createModeBox
+     * @private
+     */
+    _createModeBox(): void;
+
+    /**
+     * @static
+     * @method _createGameFontLoader
+     * @private
+     */
+    _createGameFontLoader(): void;
+
+    /**
+     * @static
+     * @method _createFontLoader
+     * @param {String} name
+     * @private
+     */
+    _createFontLoader(name: string): void;
+
+    /**
+     * @static
+     * @method _centerElement
+     * @param {HTMLElement} element
+     * @private
+     */
+    _centerElement(element: HTMLElement): void;
+
+    /**
+     * @static
+     * @method _disableTextSelection
+     * @private
+     */
+    _disableTextSelection(): void;
+
+    /**
+     * @static
+     * @method _disableContextMenu
+     * @private
+     */
+    _disableContextMenu(): void;
+
+    /**
+     * @static
+     * @method _applyCanvasFilter
+     * @private
+     */
+    _applyCanvasFilter(): void;
+
+    /**
+     * @static
+     * @method _onVideoLoad
+     * @private
+     */
+    _onVideoLoad(): void;
+
+    /**
+     * @static
+     * @method _onVideoError
+     * @private
+     */
+    _onVideoError(): void;
+
+    /**
+     * @static
+     * @method _onVideoEnd
+     * @private
+     */
+    _onVideoEnd(): void;
+
+    /**
+     * @static
+     * @method _updateVisibility
+     * @param {Boolean} videoVisible
+     * @private
+     */
+    _updateVisibility(videoVisible: boolean): void;
+
+    /**
+     * @static
+     * @method _isVideoVisible
+     * @return {Boolean}
+     * @private
+     */
+    _isVideoVisible(): void;
+
+    /**
+     * @static
+     * @method _setupEventHandlers
+     * @private
+     */
+    _setupEventHandlers(): void;
+
+    /**
+     * @static
+     * @method _onWindowResize
+     * @private
+     */
+    _onWindowResize(): void;
+
+    /**
+     * @static
+     * @method _onKeyDown
+     * @param {KeyboardEvent} event
+     * @private
+     */
+    _onKeyDown(event: KeyboardEvent): void;
+
+    /**
+     * @static
+     * @method _switchFPSMeter
+     * @private
+     */
+    _switchFPSMeter(): void;
+
+    /**
+     * @static
+     * @method _switchStretchMode
+     * @return {Boolean}
+     * @private
+     */
+    _switchStretchMode(): void;
+
+    /**
+     * @static
+     * @method _switchFullScreen
+     * @private
+     */
+    _switchFullScreen(): void;
+
+    /**
+     * @static
+     * @method _isFullScreen
+     * @return {Boolean}
+     * @private
+     */
+    _isFullScreen(): boolean;
+
+    /**
+     * @static
+     * @method _requestFullScreen
+     * @private
+     */
+    _requestFullScreen(): void;
+
+    /**
+     * @static
+     * @method _cancelFullScreen
+     * @private
+     */
+    _cancelFullScreen(): void;
 }
 declare var Graphics: GraphicsStatic;
+
+
 
 declare class Bitmap {
     constructor(width?: number, height?: number);
@@ -18,17 +632,18 @@ declare class Bitmap {
     fillAll(color: string);
 }
 
-declare class Sprite {
+declare class Sprite extends PIXI.Sprite {
     constructor(bitmap?: Bitmap);
     bitmap: Bitmap;
     x: number;
     y: number;
     update();
+    protected mikemike(): number;
 }
 
 declare class Stage {
     constructor();
-    addChild(childe: any);
+    addChild(childe: any): void;
 }
 
 
@@ -139,7 +754,7 @@ interface String {
     padZero(length: number): string;
 }
 
-declare class Point {
+declare class Point extends PIXI.Point {
     /**
      * The point class.
      *
@@ -167,7 +782,15 @@ declare class Point {
     y: number;
 }
 
-declare class Rectangle {
+declare class Rectangle extends PIXI.Rectangle {
+    /**
+     * @static
+     * @property emptyRectangle
+     * @type Rectangle
+     * @private
+     */
+    static emptyRectangle: Rectangle;
+
     /**
      * The rectangle class.
      *
@@ -211,14 +834,6 @@ declare class Rectangle {
      * @type Number
      */
     height: number;
-
-    /**
-     * @static
-     * @property emptyRectangle
-     * @type Rectangle
-     * @private
-     */
-    static emptyRectangle: Rectangle;
 }
 
 declare class ScreenSprite {
@@ -243,14 +858,14 @@ declare class ScreenSprite {
      *
      * @method setBlack
      */
-    setBlack();
+    setBlack(): void;
 
     /**
      * Sets white to the color of the screen sprite.
      *
      * @method setWhite
      */
-    setWhite();
+    setWhite(): void;
 
     /**
      * Sets the color of the screen sprite by values.
@@ -260,14 +875,14 @@ declare class ScreenSprite {
      * @param {Number} g The green value in the range (0, 255)
      * @param {Number} b The blue value in the range (0, 255)
      */
-    setColor(r?: number, g?: number, b?: number);
+    setColor(r?: number, g?: number, b?: number): void;
 
     /**
      * @method _renderCanvas
      * @param {Object} renderSession
      * @private
      */
-    _renderCanvas(renderSession: any);
+    protected _renderCanvas(renderSession: any): void;
 }
 
 interface JsonExStatic {
